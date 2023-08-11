9a0cde956cf00778b2133417ab9b30797e2f2d20
Andres <andres@deadline.com.mx>
2023-08-11 12:10:01 -0600

    Basic multipage setup

---
+++ src/app/contacto/page.js
@@ -0,0 +1,12 @@
+import styles from "./contacto.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <section className={styles.header}></section>
+      <section className={styles.main}>
+        <p>Contacto page</p>
+      </section>
+    </>
+  );
+}
---
+++ src/app/nosotros/page.js
@@ -0,0 +1,12 @@
+import styles from "./nosotros.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <section className={styles.header}></section>
+      <section className={styles.main}>
+        <p>Nosotros page</p>
+      </section>
+    </>
+  );
+}
